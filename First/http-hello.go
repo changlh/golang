ed3b3fe62e4143f7fe30adc7e20d3b907c07bd94
nealchang <neal_chang@hotmail.com>
2018-01-05 13:19:38 +0800

    add time server

--- First/http-hello.go
+++ First/http-hello.go
@@ -5,11 +5,11 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter , r *http.Request){
-	fmt.Fprintf(w,"Hello, golang!")
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s", r.RemoteAddr)
 }
 
-func main(){
-	http.HandleFunc("/" , handler)
-	http.ListenAndServe(":8800",nil)
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8800", nil)
 }
