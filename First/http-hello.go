e143a41da301673b8e35134f8f9ec3118f24a16f
nealchang <neal_chang@hotmail.com>
2018-01-07 23:48:37 +0800

    add time server

--- First/http-hello.go
+++ First/http-hello.go
@@ -6,10 +6,14 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello! Your IP and Port is : %s", r.RemoteAddr)
+	fmt.Fprintf(w, "Hello! Access IP and Port is : %s", r.RemoteAddr)
+}
+func handler1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello! %s ", r.URL.Path)
 }
 
 func main() {
+	http.HandleFunc("/path/", handler1)
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8800", nil)
 }
