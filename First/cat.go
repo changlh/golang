946209ca6913bdb22d0776a0a004d8dcc76a74ff
nealchang <neal_chang@hotmail.com>
2018-01-09 09:14:12 +0800

    实现cat命令 读取文件

---
+++ First/cat.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	//"io/ioutil"
+	"fmt"
+	"os"
+	"io/ioutil"
+)
+//读取文件内容
+func readfile(filename string) {
+	filecont , err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(filecont))
+}
+
+func main(){
+	//判断是否输入参数
+	if len(os.Args) == 1 {
+		fmt.Println("please input args")
+		return
+	}
+	a := os.Args[1]
+	//fmt.Println(a)
+	readfile(a)
+}
