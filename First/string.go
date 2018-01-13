409a9b741dae9f99f939ba06acf9a1db9474dd37
nealchang <neal_chang@hotmail.com>
2018-01-13 12:50:45 +0800

    字符替换

---
+++ First/string.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "hello,world"
+	fmt.Println(s)
+
+	//只能替换成一个字符
+	s1 := []byte(s)
+	s1[1] = 'l'		//只能用单引号
+	s = string(s1)
+	fmt.Println(s)
+
+	//可以替换为多个字符
+	s2 := "hello,world"
+	s =  "hhh" + s2[1:]
+	fmt.Println(s)
+}
