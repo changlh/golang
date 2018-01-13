9622f0ec459ebbf5813efb382677804198785cad
nealchang <neal_chang@hotmail.com>
2018-01-13 16:03:51 +0800

    增加两种字符串翻转

--- First/string.go
+++ First/string.go
@@ -1,6 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func reverse(str string) string {
+	str1 := []rune(str)
+	str2 := make([]rune, len(str1))
+	for i := 0; i < len(str1); i++ {
+		str2[i] = str1[len(str1)-i-1]
+	}
+	return string(str2)
+}
+func reverse1(str string) string {
+	str1 := []rune(str)
+	for i := 0; i < len(str1)/2; i++ {
+		str1[i], str1[len(str1)-i-1] = str1[len(str1)-i-1], str1[i]
+	}
+	return string(str1)
+}
 
 func main() {
 	s := "hello,world"
@@ -8,12 +26,17 @@ func main() {
 
 	//只能替换成一个字符
 	s1 := []byte(s)
+	fmt.Println(s1)
 	s1[1] = 'l'		//只能用单引号
 	s = string(s1)
 	fmt.Println(s)
 
 	//可以替换为多个字符
 	s2 := "hello,world"
-	s = "hhh" + s2[1:]		//1:表示1到结尾 
-	fmt.Println(s)
+	s = "hhh" + s2[1:]		//1:表示1到结尾
+	//fmt.Println(s)
+	fmt.Println(reverse(s))
+
+	s3 := "你好，世界"
+	fmt.Println(reverse1(s3))
 }
